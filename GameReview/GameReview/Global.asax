183f5f8652f530a1a3285ea1e87ff01c2870c669
hmkada01 <P2429815@my365.dmu.ac.uk>
2020-03-20 16:03:00 +0000

    New MVC solution

---
+++ GameReview/GameReview/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GameReview.MvcApplication" Language="C#" %>
---
+++ GameReview/GameReview/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameReview.Startup))]
+namespace GameReview
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
