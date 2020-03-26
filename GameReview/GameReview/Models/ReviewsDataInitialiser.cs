b508494d3bc3d36535b06ceae5ea6d022bfb00cb
Mohamed Outeh <P2436635@my365.dmu.ac.uk>
2020-03-26 13:54:10 +0000

    intial commit

--- GameReview/GameReview/Models/ReviewsDataInitialiser.cs
+++ GameReview/GameReview/Models/ReviewsDataInitialiser.cs
@@ -36,45 +36,6 @@ namespace GameReview.Models
             Rev2.Ratings = Convert.ToInt32(4);
             context.Reviews.Add(Rev2);
 
-            //third review
-            Reviews Rev3 = new Reviews();
-            Rev3.ReviewsId = 3;
-            Rev3.ReviewTitle = "Best Battle Royale game!";
-            Rev3.GameId = 3;
-            Rev3.GameTitle = "Fornite";
-            Rev3.GameGenre = "Action, Battle Royale, Survival";
-            Rev3.Review = "What sets Fortnite apart from its competitors in the Battle Royale industry is that there is constant change and new content. This encourages players to take part in easter eggs, rank rewards and more whilst offering an enjoyable and smooth gaming experience.";
-            Rev3.ReviewAuthour = "Anzilla365";
-            Rev3.Price = Convert.ToInt32(0);
-            Rev3.Ratings = Convert.ToInt32(10);
-            context.Reviews.Add(Rev3);
-
-            //fourth review
-            Reviews Rev4 = new Reviews();
-            Rev4.ReviewsId = 4;
-            Rev4.ReviewTitle = "Cars and football...";
-            Rev4.GameId = 4;
-            Rev4.GameTitle = "Rocket League";
-            Rev4.GameGenre = "Sports";
-            Rev4.Review = "The idea of playing football in cars is very unique but I must say the approach taken in Rocket League is rather impressive. The mechanincs of the game and the fact that it is something very diferent from what we're used to make it a top class game, unfortunely the match making can occasionaly let you down.";
-            Rev4.ReviewAuthour = "A_Pro";
-            Rev4.Price = Convert.ToInt32(15);
-            Rev4.Ratings = Convert.ToInt32(7);
-            context.Reviews.Add(Rev4);
-
-            //fifth review
-            Reviews Rev5 = new Reviews();
-            Rev5.ReviewsId = 5;
-            Rev5.ReviewTitle = "Rockstar never disappoints";
-            Rev5.GameId = 5;
-            Rev5.GameTitle = "Grand Theft Auto 5";
-            Rev5.GameGenre = "Action, Adventure";
-            Rev5.Review = "The latest installation in the Grand Theft Auto series sees three new characters on a heist journey, filled with action, adventure and amusing gameplay along the way. The game features online/multiplayer modes that include a range of activites for players to partake in. It never lacks fun as it often features new additions to the game. Definitely one of Rockstars best games.";
-            Rev5.ReviewAuthour = "Tafnino_ST";
-            Rev5.Price = Convert.ToInt32(49);
-            Rev5.Ratings = Convert.ToInt32(10);
-            context.Reviews.Add(Rev5);
-
             base.Seed(context);
         }
     }
